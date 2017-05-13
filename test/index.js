29016900c04b794b7c6751b05d0ef1d9b1914dbe
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-05-13 16:09:51 -0700

    New: Initial implementation based on vinyl-fs

---
+++ test/index.js
@@ -0,0 +1,168 @@
+'use strict';
+
+var os = require('os');
+var path = require('path');
+
+var fs = require('graceful-fs');
+var miss = require('mississippi');
+var expect = require('expect');
+var rimraf = require('rimraf');
+
+var mkdirpStream = require('../');
+
+var pipe = miss.pipe;
+var from = miss.from;
+var concat = miss.concat;
+
+describe('mkdirpStream', function() {
+
+  var MASK_MODE = parseInt('7777', 8);
+  var isWindows = (os.platform() === 'win32');
+
+  var outputBase = path.join(__dirname, './out-fixtures');
+  var outputDirpath = path.join(outputBase, './foo');
+
+  function cleanup(done) {
+    this.timeout(20000);
+
+    expect.restoreSpies();
+
+    // Async del to get sort-of-fix for https://github.com/isaacs/rimraf/issues/72
+    rimraf(outputBase, done);
+  }
+
+  function masked(mode) {
+    return mode & MASK_MODE;
+  }
+
+  function statMode(outputPath) {
+    return masked(fs.lstatSync(outputPath).mode);
+  }
+
+  function applyUmask(mode) {
+    if (typeof mode !== 'number') {
+      mode = parseInt(mode, 8);
+    }
+
+    return (mode & ~process.umask());
+  }
+
+  beforeEach(cleanup);
+  afterEach(cleanup);
+
+  beforeEach(function(done) {
+    fs.mkdir(outputBase, function(err) {
+      if (err) {
+        return done(err);
+      }
+
+      // Linux inherits the setgid of the directory and it messes up our assertions
+      // So we explixitly set the mode to 777 before each test
+      fs.chmod(outputBase, '777', done);
+    });
+  });
+
+  it('exports a main function, .obj and .withMode methods', function(done) {
+    expect(typeof mkdirpStream).toEqual('function');
+    expect(typeof mkdirpStream.obj).toEqual('function');
+    done();
+  });
+
+  it('takes a string to create', function(done) {
+
+    function assert() {
+      expect(statMode(outputDirpath)).toExist();
+    }
+
+    pipe([
+      from(['test']),
+      mkdirpStream(outputDirpath),
+      concat(assert),
+    ], done);
+  });
+
+  it('takes a resolver function that receives chunk', function(done) {
+
+    function resolver(chunk, cb) {
+      expect(chunk).toEqual('test');
+      cb(null, outputDirpath);
+    }
+
+    function assert() {
+      expect(statMode(outputDirpath)).toExist();
+    }
+
+    pipe([
+      from(['test']),
+      mkdirpStream(resolver),
+      concat(assert),
+    ], done);
+  });
+
+  it('can pass a mode as the 3rd argument to the resolver callback', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var mode = applyUmask('700');
+
+    function resolver(chunk, cb) {
+      expect(chunk).toEqual('test');
+      cb(null, outputDirpath, mode);
+    }
+
+    function assert() {
+      expect(statMode(outputDirpath)).toEqual(mode);
+    }
+
+    pipe([
+      from(['test']),
+      mkdirpStream(resolver),
+      concat(assert),
+    ], done);
+  });
+
+  it('can pass an error as the 1st argument to the resolver callback to error', function(done) {
+
+    function resolver(chunk, cb) {
+      cb(new Error('boom'));
+    }
+
+    function notExists() {
+      statMode(outputDirpath);
+    }
+
+    function assert(err) {
+      expect(err).toExist();
+      expect(notExists).toThrow();
+      done();
+    }
+
+    pipe([
+      from(['test']),
+      mkdirpStream(resolver),
+      concat(),
+    ], assert);
+  });
+
+  it('works with objectMode', function(done) {
+
+    function resolver(chunk, cb) {
+      expect(typeof chunk).toEqual('object');
+      expect(chunk.dirname).toExist();
+      cb(null, chunk.dirname);
+    }
+
+    function assert() {
+      expect(statMode(outputDirpath)).toExist();
+    }
+
+    pipe([
+      from.obj([{ dirname: outputDirpath }]),
+      mkdirpStream.obj(resolver),
+      concat(assert),
+    ], done);
+  });
+
+});
---
+++ test/mkdirp.js
@@ -0,0 +1,265 @@
+'use strict';
+
+var os = require('os');
+var path = require('path');
+
+var fs = require('graceful-fs');
+var expect = require('expect');
+var rimraf = require('rimraf');
+
+var mkdirp = require('../mkdirp');
+
+describe('mkdirp', function() {
+
+  var MASK_MODE = parseInt('7777', 8);
+  var DEFAULT_DIR_MODE = parseInt('0777', 8);
+  var isWindows = (os.platform() === 'win32');
+
+  var outputBase = path.join(__dirname, './out-fixtures');
+  var outputDirpath = path.join(outputBase, './foo');
+  var outputNestedPath = path.join(outputDirpath, './test.txt');
+  var outputNestedDirpath = path.join(outputDirpath, './bar/baz/');
+  var contents = 'Hello World!\n';
+
+  function cleanup(done) {
+    this.timeout(20000);
+
+    expect.restoreSpies();
+
+    // Async del to get sort-of-fix for https://github.com/isaacs/rimraf/issues/72
+    rimraf(outputBase, done);
+  }
+
+  function masked(mode) {
+    return mode & MASK_MODE;
+  }
+
+  function statMode(outputPath) {
+    return masked(fs.lstatSync(outputPath).mode);
+  }
+
+  function applyUmask(mode) {
+    if (typeof mode !== 'number') {
+      mode = parseInt(mode, 8);
+    }
+
+    return (mode & ~process.umask());
+  }
+
+  beforeEach(cleanup);
+  afterEach(cleanup);
+
+  beforeEach(function(done) {
+    fs.mkdir(outputBase, function(err) {
+      if (err) {
+        return done(err);
+      }
+
+      // Linux inherits the setgid of the directory and it messes up our assertions
+      // So we explixitly set the mode to 777 before each test
+      fs.chmod(outputBase, '777', done);
+    });
+  });
+
+  it('makes a single directory', function(done) {
+    mkdirp(outputDirpath, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputDirpath)).toExist();
+
+      done();
+    });
+  });
+
+  it('makes single directory w/ default mode', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var defaultMode = applyUmask(DEFAULT_DIR_MODE);
+
+    mkdirp(outputDirpath, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputDirpath)).toEqual(defaultMode);
+
+      done();
+    });
+  });
+
+  it('makes multiple directories', function(done) {
+    mkdirp(outputNestedDirpath, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputNestedDirpath)).toExist();
+
+      done();
+    });
+  });
+
+  it('makes multiple directories w/ default mode', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var defaultMode = applyUmask(DEFAULT_DIR_MODE);
+
+    mkdirp(outputNestedDirpath, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputNestedDirpath)).toEqual(defaultMode);
+
+      done();
+    });
+  });
+
+  it('makes directory with custom mode', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var mode = applyUmask('700');
+
+    mkdirp(outputDirpath, mode, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputDirpath)).toEqual(mode);
+
+      done();
+    });
+  });
+
+  it('can create a directory with setgid permission', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var mode = applyUmask('2700');
+
+    mkdirp(outputDirpath, mode, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputDirpath)).toEqual(mode);
+
+      done();
+    });
+  });
+
+  it('does not change directory mode if exists and no mode given', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var mode = applyUmask('700');
+
+    mkdirp(outputDirpath, mode, function(err) {
+      expect(err).toNotExist();
+
+      mkdirp(outputDirpath, function(err2) {
+        expect(err2).toNotExist();
+        expect(statMode(outputDirpath)).toEqual(mode);
+
+        done();
+      });
+    });
+  });
+
+  it('makes multiple directories with custom mode', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var mode = applyUmask('700');
+
+    mkdirp(outputNestedDirpath, mode, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputNestedDirpath)).toEqual(mode);
+
+      done();
+    });
+  });
+
+  it('uses default mode on intermediate directories', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var intermediateDirpath = path.dirname(outputNestedDirpath);
+    var mode = applyUmask('700');
+    var defaultMode = applyUmask(DEFAULT_DIR_MODE);
+
+    mkdirp(outputNestedDirpath, mode, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputDirpath)).toEqual(defaultMode);
+      expect(statMode(intermediateDirpath)).toEqual(defaultMode);
+
+      done();
+    });
+  });
+
+  it('changes mode of existing directory', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var mode = applyUmask('700');
+    var defaultMode = applyUmask(DEFAULT_DIR_MODE);
+
+    mkdirp(outputDirpath, function(err) {
+      expect(err).toNotExist();
+      expect(statMode(outputDirpath)).toEqual(defaultMode);
+
+      mkdirp(outputDirpath, mode, function(err2) {
+        expect(err2).toNotExist();
+        expect(statMode(outputDirpath)).toEqual(mode);
+
+        done();
+      });
+    });
+  });
+
+  it('errors with EEXIST if file in path', function(done) {
+    mkdirp(outputDirpath, function(err) {
+      expect(err).toNotExist();
+
+      fs.writeFile(outputNestedPath, contents, function(err2) {
+        expect(err2).toNotExist();
+
+        mkdirp(outputNestedPath, function(err3) {
+          expect(err3).toExist();
+          expect(err3.code).toEqual('EEXIST');
+
+          done();
+        });
+      });
+    });
+  });
+
+  it('does not change mode of existing file', function(done) {
+    if (isWindows) {
+      this.skip();
+      return;
+    }
+
+    var mode = applyUmask('700');
+
+    mkdirp(outputDirpath, function(err) {
+      expect(err).toNotExist();
+
+      fs.writeFile(outputNestedPath, contents, function(err2) {
+        expect(err2).toNotExist();
+
+        var expectedMode = statMode(outputNestedPath);
+
+        mkdirp(outputNestedPath, mode, function(err3) {
+          expect(err3).toExist();
+          expect(statMode(outputNestedPath)).toEqual(expectedMode);
+
+          done();
+        });
+      });
+    });
+  });
+});
